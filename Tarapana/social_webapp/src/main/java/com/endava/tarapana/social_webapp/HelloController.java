059db7c79ad0324e081ef0ad30a8c95b6da3c2b6
Dejan Kojic <dejan.kojic@endava.com>
2016-02-25 12:47:52 +0100

    Check admin pages for logged in user

--- Tarapana/social_webapp/src/main/java/com/endava/tarapana/social_webapp/HelloController.java
+++ Tarapana/social_webapp/src/main/java/com/endava/tarapana/social_webapp/HelloController.java
@@ -1,19 +1,28 @@
 package com.endava.tarapana.social_webapp;
 
+import java.util.ArrayList;
+
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Account;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.PagedList;
-import org.springframework.social.facebook.api.Post;
+import org.springframework.social.facebook.api.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.endava.tarapana.util.TrackingEntityUtility;
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
 	private Facebook facebook;
 
@@ -31,9 +40,30 @@ public class HelloController {
 			return "redirect:/connect/facebook";
 		}
 
-		model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
-		PagedList<Post> feed = facebook.feedOperations().getFeed();
-		model.addAttribute("feed", feed);
+		User user = facebook.userOperations().getUserProfile();
+		model.addAttribute("facebookProfile", user);
+		PagedList<Account> pageList = facebook.pageOperations().getAccounts();
+
+		if (pageList != null && !pageList.isEmpty()) {
+			PagedList<Account> pagesToTrack = (PagedList<Account>) new ArrayList<Account>();
+			System.out.println("Ima strana");
+			for (Account page : pagesToTrack) {
+				if (facebook.pageOperations().isPageAdmin(page.getId())) {
+					pagesToTrack.add(page);
+				}
+			}
+			if (pagesToTrack != null && !pagesToTrack.isEmpty()) {
+				TrackingEntityUtility.storeTrackingEntities(pagesToTrack);
+			}
+			else {
+				logger.warn("User " + user.getFirstName() + " " + user.getLastName()
+						+ " doesn't have any administrated pages.");
+			}
+		}
+		else {
+			logger.warn("User " + user.getFirstName() + " " + user.getLastName() + " doesn't have any related pages.");
+		}
+
 		return "hello";
 	}
 
---
+++ Tarapana/social_webapp/src/main/java/com/endava/tarapana/util/TrackingEntityUtility.java
@@ -0,0 +1,12 @@
+package com.endava.tarapana.util;
+
+import org.springframework.social.facebook.api.Account;
+import org.springframework.social.facebook.api.PagedList;
+
+public class TrackingEntityUtility {
+
+	public static void storeTrackingEntities(PagedList<Account> pages) {
+		// TODO: Call rest end point to store tracking entity
+	}
+
+}
